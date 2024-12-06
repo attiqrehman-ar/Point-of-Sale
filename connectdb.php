c74c0c03f4961699216f171405a8b118525744ea
Zain Ali <malikzain909192@gmail.com>
2024-12-06 06:09:59 +0200

    Database connection is updated

--- connectdb.php
+++ connectdb.php
@@ -1,6 +1,6 @@
 <?php
 // Connect to the database
-$conn = mysqli_connect("localhost", "root", "", "inventory management system");
+$conn = mysqli_connect("localhost", "root", "", "inventory management");
       
 // Check connection
 if (!$conn) {
