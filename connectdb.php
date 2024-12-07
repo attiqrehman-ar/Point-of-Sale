c03cbeca846be0344454ab7bc2cba2098ae650ad
Zain Ali <malikzain909192@gmail.com>
2024-12-07 07:08:21 +0200

    Database error solve

--- connectdb.php
+++ connectdb.php
@@ -1,6 +1,6 @@
 <?php
 // Connect to the database
-$conn = mysqli_connect("localhost", "root", "", "inventory management");
+$conn = mysqli_connect("localhost", "root", "", "Point_of_sale");
       
 // Check connection
 if (!$conn) {
